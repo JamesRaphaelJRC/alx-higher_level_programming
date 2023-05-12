1ccb3a2a3240109aea6852cf6c964380479c9edf
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-11 18:40:40 -0700

    added condition for args = 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     ac = (len(sys.argv) - 1)
     if ac == 0:
         print(ac, "arguments.")
+    elif ac == 1:
+        print(ac, "argument:")
     else:
         print(ac, "arguments:")
-        for i in range(1, ac + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, ac + 1):
+        print("{}: {}".format(i, sys.argv[i]))
