55a246a498d11aa81a2a585d24f4c4844092d53f
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-12 05:59:23 -0700

    Task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    print_list_integer: Prints all integers of a list.
+    my_list: The list to be printed
+"""
+def print_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        print("Empty list")
+    else:
+        for num in my_list:
+            print("{}".format(num))
