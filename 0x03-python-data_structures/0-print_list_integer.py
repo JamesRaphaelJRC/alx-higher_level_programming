8fbd9b7e91b4d38f1a318394ca20215a01420ed9
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-12 06:12:44 -0700

    Task 0 fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,9 +3,11 @@
     print_list_integer: Prints all integers of a list.
     my_list: The list to be printed
 """
+
+
 def print_list_integer(my_list=[]):
     if len(my_list) == 0:
         print("Empty list")
     else:
         for num in my_list:
-            print("{}".format(num))
+            print("{:d}".format(num))
