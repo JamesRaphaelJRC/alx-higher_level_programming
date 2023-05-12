c85d5bacc5f56f31e7e87a5f53b9961ff4aec1ae
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-12 09:57:30 -0700

    Task 4 and 2 update

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return(my_list)
+
+    list_copy = []
+    for elem in my_list:
+        list_copy.append(elem)
+
+    list_copy[idx] = element
+    return(list_copy)
