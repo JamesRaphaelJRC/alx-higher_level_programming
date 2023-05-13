8c449880aafa21e5071c212acd9dce26accf828a
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-13 03:44:28 -0700

    Added isinstance()

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if isinstance(my_string, str):
-            new_str = ""
-            for word in my_string:
-                if word != 'c' and word != 'C':
-                    new_str += word
-            return(new_str)
+        new_str = ""
+        for word in my_string:
+            if word != 'c' and word != 'C':
+                new_str += word
+        return(new_str)
