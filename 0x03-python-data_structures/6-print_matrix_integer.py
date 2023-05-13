ce65c1b1c20d06056b35d90cd3589fc4833684c4
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-13 08:56:56 -0700

    Task 6 fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,10 @@ def print_matrix_integer(matrix=[[]]):
         for row in matrix:
             transpose_row = []
             for i in range(len(row)):
-                transpose_row.append(str(row[i]))
-            transposed.append(' '.join(transpose_row))
-        print('\n'.join(transposed))
+                transpose_row.append(row[i])
+            transposed.append(transpose_row)
+
+        for row in transposed:
+            for i in row:
+                print("{:d}".format(i), end=" ")
+            print()
