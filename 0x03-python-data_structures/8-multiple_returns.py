7ab8625deb700f71cfe98f906210e2d48df73b88
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-14 09:35:41 -0700

    Task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+    multiple_returns - returns a tuple with the length of a string and its
+    first character.
+'''
+
+
+def multiple_returns(sentence):
+    return((len(sentence)), sentence[0])
