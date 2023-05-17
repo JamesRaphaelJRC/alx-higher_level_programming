d84cbc1b90b8d9ddc01fd10be5465f9bd59a0b64
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-17 10:02:27 -0700

    Task 1 added comment

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if isinstance(my_list, list):
+        ''' Replaces search with replace at every occurrence in my_list'''
         return(list(map(lambda x: replace if x == search else x, my_list)))
