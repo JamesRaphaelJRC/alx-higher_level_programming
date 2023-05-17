1d599021f8c180e344ec0488121a89e5c67813a0
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-17 11:59:27 -0700

    Task 5 fix

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     ''' Returns the number of keys in a dictionary without importing nothing'''
-    for i, key in enumerate(a_dictionary):
-        pass
-    return(i + 1)
+    return(len(a_dictionary))
