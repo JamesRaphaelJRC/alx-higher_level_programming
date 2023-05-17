273eb43f6c7aea14ac26eff78912feeaf9d71f07
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-17 13:42:55 -0700

    Added long format

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    ''' Returns a new dictiionary with all values multiplied by 2'''
+    ''' Returns a new dictiionary with all values multiplied by 2
+    Long format:
+     new_dictionary = {}
+    for key in a_dictionary:
+        new_dictionary[key] = a_dictionary[key] * 2
+    return new_dictionary
+    '''
     return({k: a_dictionary[k] * 2 for k in a_dictionary})
