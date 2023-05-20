4b9dc5b39979f310ca2bf26ee9ae17df22aad85b
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-20 01:57:42 -0700

    Task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if isinstance(my_list, list):
+        if my_list is None:
+            return (0)
+        numerator, denominator = 0, 0
+        i, j = 0, 1
+        for num_set in my_list:
+            numerator += num_set[i] * num_set[j]
+            denominator += num_set[j]
+        return (numerator / denominator)
