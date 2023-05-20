e9935d51f79546acf011551e9259b51858fa4109
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-20 02:19:01 -0700

    Task 100 modified

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 def weight_average(my_list=[]):
     if not isinstance(my_list, list) or len(my_list) == 0:
-        return 0
-    
+        return (0)
+
     numerator = sum(x * y for x, y in my_list)
     denominator = sum(y for _, y in my_list)
-    
-    return numerator / denominator
+
+    return (numerator / denominator)
