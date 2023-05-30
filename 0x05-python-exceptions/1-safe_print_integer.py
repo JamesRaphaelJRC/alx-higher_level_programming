1501a2094e27f3c40d2d7e206db0144f5959e551
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-05-29 19:17:46 -0700

    Added typeerror excepption

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,7 +5,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         condition = True
-    except ValueError:
+    except (ValueError, TypeError):
         condition = False
 
     return (condition)
