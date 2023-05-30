edb758c175d2fa7e962de8acb7f7e60611bd424f
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-05-30 11:08:33 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+''' A class 'Square' '''
+
+
+class Square:
+    ''' Defines a square based on:
+        size: A private instance attribute.
+    '''
+    def __init__(self, size=0):
+        self.__size = size
