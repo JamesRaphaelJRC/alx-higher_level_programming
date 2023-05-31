8f65b00b8873c612f61157f80a2b1bbc1c818f7b
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-05-31 07:57:05 -0700

    task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+''' Defines a class "Square' '''
+
+
+class Square:
+    ''' Initialize Square with a private instance size and
+        position (a turple)
+    '''
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    ''' Get/set current size and position of Square object '''
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) and value < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    ''' returns the current square area '''
+    def area(self):
+        return (self.__size ** 2)
+
+    ''' Prints in stdout the square with the character # '''
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                j = 0
+                while j < self.__position[0]:
+                    if self.__position[1] <= 0:
+                        print(" ", end='')
+                    j += 1
+                print("#" * self.__size)
