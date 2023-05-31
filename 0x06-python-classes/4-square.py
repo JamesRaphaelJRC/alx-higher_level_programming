6ae951c2e057f2d41cb6cfd8adadfcdd77a7090e
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-05-30 22:04:49 -0700

    fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,21 +8,19 @@ class Square:
     ''' Initialize Square with a private instance int 'size' '''
 
     def __init__(self, size=0):
-
-
         self.__size = size
 
-    @property
     ''' retrives value of size '''
+    @property
     def size(self):
         return (self.__size)
-    
-    @size.setter
+
     ''' sets the value of size '''
+    @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
