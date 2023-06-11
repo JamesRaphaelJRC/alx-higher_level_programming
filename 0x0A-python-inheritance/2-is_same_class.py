cd819e95600a9a62f63fed3449fefa326f3f47e8
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-06-10 20:25:56 -0700

    Task 1 fix

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,8 +4,8 @@
 
 
 def is_same_class(obj, a_class):
-    ''' eturns 'True' if the object is exactly an instance of the specified class
-        otherwise 'False'
+    ''' eturns 'True' if the object is exactly an instance of the specified
+        class otherwise 'False'
 
         obj: The object to be checked
         a_class: The class used for comparism
