afbcd8282f15a6d02e444c28caa1687243a16d53
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-06-13 11:05:41 -0700

    task o fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     ''' Reads a textfile (UTF8) and prints it to stdout.'''
     if type(filename) is str:
         with open(filename, 'r', encoding="UTF-8") as f:
-            print(f.read())
+            print(f.read(), end='', end='')
