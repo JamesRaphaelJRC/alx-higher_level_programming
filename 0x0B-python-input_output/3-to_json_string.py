b2b399f7f36a218eb4f08212bfb9d0beba678c2b
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-06-13 19:39:43 -0700

    Task 3 pycodestyle fixed

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 ''' Defines a function, 'to_json_string'.'''
 import json
 
+
 def to_json_string(obj):
     ''' Returns the JSON representation of an object(string).'''
     return (json.dumps(obj))
