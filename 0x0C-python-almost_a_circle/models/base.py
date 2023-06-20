dfa51d9688c4d3da19be85e91e47e86bf7612189
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-06-20 04:31:58 -0700

    Fixed to_json_string return

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,7 +27,7 @@ class Base:
                 is retuned.
         '''
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
