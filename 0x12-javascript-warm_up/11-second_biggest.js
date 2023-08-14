bb29a523888d1325de47a54cab26d57c74bfaa14
James Raphael JRC <jamesraphaeljrc@gmail.com>
2023-08-14 22:13:53 +0000

    updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const length = process.argv.length - 2;
-if (length === 0) {
-  console.log(0);
-} else if (length === 1) {
+if (length < 2) {
   console.log(0);
 } else {
   let j = 0;
