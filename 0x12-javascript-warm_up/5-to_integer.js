ef21ba2509ecfad8d5205d93a4d0a0b7140acedf
James Raphael JRC <jamesraphaeljrc@gmail.com>
2023-08-14 19:52:57 +0000

    conversion to Number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const arg = Math.floor(Number(process.argv[2]));
 if (isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', arg);
+  console.log(`My number: ${arg}`);
 }
