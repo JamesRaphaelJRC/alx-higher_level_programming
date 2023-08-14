c578f76432232349ab1fc0b89f92f844005ec023
James Raphael JRC <jamesraphaeljrc@gmail.com>
2023-08-14 22:09:06 +0000

    second biggest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const length = process.argv.length - 2;
+if (length === 0) {
+  console.log(0);
+} else if (length === 1) {
+  console.log(0);
+} else {
+  let j = 0;
+  const argList = [];
+  for (let i = 2; i < length + 2; i++) {
+    argList[j] = Number(process.argv[i]);
+    j++;
+  }
+  const maximum = Math.max(...argList);
+  const newArgList = argList.filter(element => element < maximum);
+  console.log(Math.max(...newArgList));
+}
