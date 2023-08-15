57688167430ac84f3b6362d42691c7040fe98e2b
James Raphael JRC <jamesraphaeljrc@gmail.com>
2023-08-15 09:16:21 +0000

    factorial update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial (num) {
   if (num === 0) return 1;
-  if (num < 1) return 0;
+  if (num < 0) return 0;
   return num * factorial(num - 1);
 }
-const num = Math.floor(Number(process.argv[2]));
+let num = Math.floor(Number(process.argv[2]));
 if (isNaN(num)) num = 0;
 console.log(factorial(num));
