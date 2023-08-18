2c772511b63558bd226603c77cb04d2004b0f2b3
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-08-18 08:36:48 -0700

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,9 @@ import MySQLdb
 
 
 ''' Lists all states from the database 'hbtn_0e_0_usa' '''
+if len(sys.argv) != 4:
+    sys.exit(1)
+
 username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
 db = MySQLdb.connect(host='localhost', port=3306, user=username,
@@ -18,7 +21,3 @@ for state in allStates:
 
 cursor.close()
 db.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        sys.exit(1)
