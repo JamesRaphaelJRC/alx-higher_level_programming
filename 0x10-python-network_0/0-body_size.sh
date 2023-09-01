7f7cf0c5362025f8ea1c0ed208e1339d055e22f2
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-09-01 04:34:28 -0700

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the size of the
-# body of the response
-curl -s "$1" | wc -l
+# Sends a request to a URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
